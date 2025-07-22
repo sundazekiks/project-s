63d73301a0149b6c5e344e0f3546ddc4cd572568
Travis Abuton <abutontravis1@gmail.com>
2025-07-22 10:45:18 -0400

    1st Commit to test out

---
+++ index.js
@@ -0,0 +1,26 @@
+let mainBg = document.querySelector(".main");
+let mainText = document.querySelector(".main-text");
+let specialMsg = document.querySelector(".special-msg");
+
+const imgs = document.querySelectorAll("img");
+
+const imgLinks = [
+    "images/1p.jpg",
+    "images/2p.jpg",
+    "images/3p.jpg",
+    "images/4p.jpg",
+    "images/5p.jpg"
+]
+
+imgs.forEach(function(img, index) {
+  img.addEventListener('click', () => {
+    console.log("Hello World", index);
+
+    mainBg.style.backgroundImage = imgLinks[index];
+    
+    
+    
+  });
+  
+
+});
